5e9f36bb0cb82daf4036f0c38670741a1c38eedb
jerry.zeng <1090211579@qq.com>
2019-12-02 18:06:23 +0800

    role

--- src/views/system/role/index.vue
+++ src/views/system/role/index.vue
@@ -2,18 +2,19 @@
   <div>
     <list-page
       uri="/system/role"
-      dialogTitle="角色"
-      dialogWidth="500px"
-      :columns="columns" 
-      :modelRules="modelRules"
-      :isPaging="false"
-      :model.sync="model">
+      dialog-title="角色"
+      dialog-width="500px"
+      :columns="columns"
+      :model-rules="modelRules"
+      :is-paging="false"
+      :model.sync="model"
+    >
       <template>
         <el-form-item prop="name" label="角色名称">
-          <el-input v-model="model.Name"></el-input>
+          <el-input v-model="model.name" />
         </el-form-item>
         <el-form-item prop="memo" label="备注">
-          <el-input v-model="model.Memo"></el-input>
+          <el-input v-model="model.memo" />
         </el-form-item>
       </template>
     </list-page>
@@ -36,31 +37,31 @@ export default {
       model: {}
     }
   },
-  methods: {
-  },
   created() {
     this.columns = [
       {
         type: 'selection',
         width: 55
-      },{
+      }, {
         prop: 'id',
         label: '编码',
         width: 100
-      },{
+      }, {
         prop: 'name',
         label: '名称',
         width: 100
-      },{
+      }, {
         prop: 'memo',
         label: '备注'
-      },{
+      }, {
         type: '_opt',
         label: '操作',
         width: 150,
-        actions: ['edit','delete']
+        actions: ['edit', 'delete']
       }
     ]
+  },
+  methods: {
   }
 }
 </script>>
