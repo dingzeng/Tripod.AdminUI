bbf96b3976654d276c16cb8022afd44b238c8c15
jerry <1090211579@qq.com>
2019-12-13 17:41:51 +0800

    branch

--- src/views/archive/branch/branch.vue
+++ src/views/archive/branch/branch.vue
@@ -1,13 +1,15 @@
 <template>
   <div>
-    <list-page 
+    <list-page
       uri="archive/branch"
-      :queryParams="queryParams"
+      dialog-title="机构"
+      :query-params="queryParams"
       :columns="columns"
-      :model.sync="model">
+      :model.sync="model"
+    >
       <template slot="queryForm">
         <el-form-item prop="keyword">
-          <el-input v-model="queryParams.keyword" placeholder="机构编码/名称"></el-input>
+          <el-input v-model="queryParams.keyword" placeholder="机构编码/名称" />
         </el-form-item>
       </template>
       <template slot="mainLeft">
@@ -16,80 +18,103 @@
           class="filter-tree"
           default-expand-all
           :data="branchTreeData"
-          @node-click="handleNodeClick">
-        </el-tree>
+          @node-click="handleNodeClick"
+        />
       </template>
       <template>
-        <el-row :gutter="10">
+        <el-row>
           <el-col :span="8">
             <el-form-item prop="parentId" required label="上级机构">
-              <ref-input v-model="model.parentId" type="branch" :label.sync="model.parentName"></ref-input>
+              <ref-input v-model="model.parentId" type="branch" :label.sync="model.parentName" />
             </el-form-item>
           </el-col>
           <el-col :span="8">
             <el-form-item prop="id" required label="机构编码">
-              <el-input v-model="model.id"></el-input>
+              <el-input v-model="model.id" />
             </el-form-item>
           </el-col>
           <el-col :span="8">
             <el-form-item prop="type" required label="机构类型">
               <el-select v-model="model.type">
-                <el-option 
+                <el-option
                   v-for="(label,key) in branchType"
                   :key="key"
-                  :value="key"
-                  :label="label">
-                </el-option>
+                  :value="Number(key)"
+                  :label="label"
+                />
               </el-select>
             </el-form-item>
           </el-col>
         </el-row>
-        <el-row :gutter="10">
+        <el-row>
           <el-col :span="8">
             <el-form-item prop="name" required label="机构名称">
-              <el-input v-model="model.name"></el-input>
+              <el-input v-model="model.name" />
             </el-form-item>
           </el-col>
           <el-col :span="8">
             <el-form-item prop="shortName" required label="机构简称">
-              <el-input v-model="model.shortName"></el-input>
+              <el-input v-model="model.shortName" />
             </el-form-item>
           </el-col>
         </el-row>
-        <el-divider></el-divider>
-        <el-row :gutter="10">
+        <el-divider />
+        <el-row>
           <el-col :span="8">
             <el-form-item prop="contactsName" label="联系人">
-              <el-input v-model="model.contactsName"></el-input>
+              <el-input v-model="model.contactsName" />
             </el-form-item>
           </el-col>
           <el-col :span="8">
-            <el-form-item prop="contactsMobile" label="联系人手机号">
-              <el-input v-model="model.contactsMobile"></el-input>
+            <el-form-item prop="contactsMobile" label="手机号">
+              <el-input v-model="model.contactsMobile" />
             </el-form-item>
           </el-col>
           <el-col :span="8">
-            <el-form-item prop="contactsTel" label="联系电话">
-              <el-input v-model="model.contactsTel"></el-input>
-            </el-form-item>
-          </el-col>
-        </el-row>
-        <el-row :gutter="10">
-          <el-col :span="8">
-            <el-form-item prop="contactsEmail" label="邮箱">
-              <el-input v-model="model.contactsEmail"></el-input>
-            </el-form-item>
-          </el-col>
-          <el-col :span="8">
-            <el-form-item prop="address" label="地址">
-              <el-input v-model="model.address"></el-input>
+            <el-form-item prop="contactsTel" label="电话号码">
+              <el-input v-model="model.contactsTel" />
             </el-form-item>
           </el-col>
         </el-row>
         <el-row>
-          <el-col>
+          <el-col :span="8">
+            <el-form-item prop="contactsEmail" label="邮箱">
+              <el-input v-model="model.contactsEmail" />
+            </el-form-item>
+          </el-col>
+          <el-col :span="8">
+            <el-form-item prop="address" label="地址">
+              <el-input v-model="model.address" />
+            </el-form-item>
+          </el-col>
+        </el-row>
+        <el-divider />
+        <el-row>
+          <el-col :span="8">
+            <el-form-item prop="createOperName" label="创建人">
+              <el-input v-model="model.createOperName" readonly />
+            </el-form-item>
+          </el-col>
+          <el-col :span="8">
+            <el-form-item prop="createTime" label="创建时间">
+              <el-input v-model="model.createTime" readonly />
+            </el-form-item>
+          </el-col>
+          <el-col :span="8">
+            <el-form-item prop="lastUpdateOperName" label="最后修改人">
+              <el-input v-model="model.lastUpdateOperName" readonly />
+            </el-form-item>
+          </el-col>
+        </el-row>
+        <el-row>
+          <el-col :span="8">
+            <el-form-item prop="lastUpdateTime" label="最后修改时间">
+              <el-input v-model="model.lastUpdateTime" readonly />
+            </el-form-item>
+          </el-col>
+          <el-col :span="8">
             <el-form-item prop="memo" label="备注">
-              <el-input v-model="model.memo"></el-input>
+              <el-input v-model="model.memo" />
             </el-form-item>
           </el-col>
         </el-row>
@@ -114,11 +139,6 @@ export default {
       modelRules: [],
       branchType,
       branchTreeData: []
-    }
-  },
-  methods: {
-    handleNodeClick(data) {
-      console.log(data)
     }
   },
   mounted() {
@@ -174,6 +194,11 @@ export default {
         actions: ['edit', 'delete']
       }
     ]
+  },
+  methods: {
+    handleNodeClick(data) {
+      console.log(data)
+    }
   }
 }
 </script>
